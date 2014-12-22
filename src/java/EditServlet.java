41eb214e1aca69c25ea5a4dcfa4bba1b6972e69e
Brijesh Patel <Brijeshrpatel9@users.noreply.github.com>
2014-12-21 20:38:13 -0600

    Update EditServlet.java

--- src/java/EditServlet.java
+++ src/java/EditServlet.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
