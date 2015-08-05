48f086566c6d1e801b5c61567a6d7ce99edee5c3
Brijesh Patel <Brijeshrpatel9@users.noreply.github.com>
2015-08-04 22:37:45 -0400

    Update ViewOrDelete.java

--- src/java/ViewOrDelete.java
+++ src/java/ViewOrDelete.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
