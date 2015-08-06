d07ceaf9d41a7f0ef03d66294b3889078c1105e8
Brijesh Patel <Brijeshrpatel9@users.noreply.github.com>
2015-08-05 23:57:43 -0400

    Update ACustListServlet.java

--- src/java/ACustListServlet.java
+++ src/java/ACustListServlet.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
@@ -20,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Brijesh Admin
  */
- 
+
 public class ACustListServlet extends HttpServlet {
 
     /**
