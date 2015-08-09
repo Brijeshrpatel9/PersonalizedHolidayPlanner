0f73a16ce59b8880afc8e0753b41c2bdbeee09fd
Brijesh Patel <Brijeshrpatel9@users.noreply.github.com>
2015-08-09 00:21:46 -0400

    Update ACustInfoServlet.java

--- src/java/ACustInfoServlet.java
+++ src/java/ACustInfoServlet.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
