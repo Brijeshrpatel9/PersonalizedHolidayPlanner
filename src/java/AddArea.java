4060daafe7e5d6bb86149a188c7a9dc2f65894df
Brijesh Patel <Brijeshrpatel9@users.noreply.github.com>
2015-08-26 23:45:06 -0400

    Update AddArea.java

--- src/java/AddArea.java
+++ src/java/AddArea.java
@@ -46,7 +46,6 @@ public class AddArea extends HttpServlet {
       
              String email = (String) session.getAttribute("Username");
            
-            
             query="insert into Vendor_Area (Vendor_ID,Dest_ID,Area) values ('"+email+"','"+d+"','"+a+"')";
              Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             Connection con = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=PHP;username=sa;password=icreate;");
