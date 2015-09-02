53dc2dfccd6fc82e452492ecd81e765e106f5e02
Brijesh Patel <Brijeshrpatel9@users.noreply.github.com>
2015-09-02 16:39:37 -0400

    Update BookedTripServlet.java

--- src/java/BookedTripServlet.java
+++ src/java/BookedTripServlet.java
@@ -33,6 +33,7 @@ public class BookedTripServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
         response.setContentType("text/html;charset=UTF-8");
         HttpSession session = request.getSession();
         ArrayList<NewTrip> booktrip;
